7e04c891c0b6e5b5754ca6b0e71aa8922f87976a
PAT CAMP <PATS0825@MSN.COM>
2018-08-20 19:11:59 -0400

    added faker, identifying fake users and adding fake users to seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_222216) do
+ActiveRecord::Schema.define(version: 2018_08_20_225822) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_222216) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "fake"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+puts "Generating my user"
 u = User.find_or_create_by(username: "pitty")
 
 u.update_attributes({
     first_name: "Pat",
-    last_name: "Camp",
-    city: "Columbus",
-    state: "Ohio",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    last_name:  "Camp",
+    city:       "Columbus",
+    state:      "Ohio",
+    bio:        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+    fake:       false,
   })
 
   u.save!
+
+
+  puts "deleting fake users"
+  User.where(fake: true).destroy_all
+
+  puts "generating fake users\n"
+  10.times do
+    putc "."
+    User.create!({
+      username:   Faker::Internet.username,
+      first_name: Faker::Name.first_name,
+      last_name:  Faker::Name.last_name,
+      city:       Faker::Address.city,
+      state:      Faker::Address.state_abbr,
+      bio:        Faker::Lorem.paragraph,
+      fake:       true,
+
+      })
+  end
+
+  puts "done"
